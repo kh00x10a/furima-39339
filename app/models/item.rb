8e90c025e266397b70010ad213601a5015344717
kh00x10a <sein.nicht04@gmail.com>
2023-05-26 19:30:14 +0900

    ActiveHashの生成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :shipping_fee_responsibility
+  belongs_to :shipping_day
+
+  validates :category_id, :condition_id, :prefecture_id, :shipping_fee_responsibility_id, :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
