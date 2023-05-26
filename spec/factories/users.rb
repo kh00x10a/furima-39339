9c7a575d0892fb48d80ce690d0239aa879b28dbb
kh00x10a <sein.nicht04@gmail.com>
2023-05-26 16:49:02 +0900

    FactoryBot編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {'shibuya三郎'}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
     last_name             {'や真ダ'}
     first_name            {'他ろ宇'}
