a306cf4e1c96a6c722162e2107d241c8b703d664
kh00x10a <sein.nicht04@gmail.com>
2023-05-27 18:07:26 +0900

    商品出品機能の実装2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,20 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :category
   belongs_to :condition
-  belongs_to :prefecture
   belongs_to :shipping_fee_responsibility
+  belongs_to :prefecture
   belongs_to :shipping_day
   has_one_attached :image
 
-  validates :category_id, :condition_id, :prefecture_id, :shipping_fee_responsibility_id, :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :name, :explanation, :image, presence: true
+
+  validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, format: { with: /\A[0-9]+\z/, message: "は半角数字のみ使用してください。" }
+
+  validates :category_id, :condition_id, :shipping_fee_responsibility_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :user
 end
