237ca002c1ff9b115de8ba94c36d01dbb043b189
kh00x10a <sein.nicht04@gmail.com>
2023-05-28 16:29:57 +0900

    items#editとitems#updateアクションの中身の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] 
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -23,10 +24,21 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.nil?
+      redirect_to new_user_session_path
+    elsif current_user != @item.user
+      redirect_to root_path
+    end
   end
 
   def update
-    
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
 
@@ -35,6 +47,7 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_fee_responsibility_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
+
 end
 
 
