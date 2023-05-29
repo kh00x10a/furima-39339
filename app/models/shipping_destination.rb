94d3c44040ad47826b02a0c39d967f28b64dd4c1
kh00x10a <sein.nicht04@gmail.com>
2023-05-29 12:25:33 +0900

    配送先入力欄のカラム名の編集と都道府県のアクティブハッシュの設定

--- app/models/shipping_destination.rb
+++ app/models/shipping_destination.rb
@@ -2,7 +2,9 @@ class ShippingDestination < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :purchase_record
-  belongs_to_active_hash :prefecture
+  belongs_to :prefecture
 
-  
+  validates :prefecture_id,  numericality: { other_than: 1 , message: "can't be blank"}
+
+
 end
