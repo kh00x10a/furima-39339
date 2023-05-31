7dc4a455dbcf89410afc7654e5cf5fdda5a54666
kh00x10a <sein.nicht04@gmail.com>
2023-05-31 13:07:54 +0900

    formオブジェクトのテストコードの記述

--- app/models/purchase_record_shipping_destination.rb
+++ app/models/purchase_record_shipping_destination.rb
@@ -7,8 +7,8 @@ class PurchaseRecordShippingDestination
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :user_id
     validates :item_id
-    validates :city, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :street_address, format: { with: /\A[\d-]+\z/, message: "は数字とハイフンのみ入力してください" }
+    validates :city, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' }
+    validates :street_address, presence: true, format: { with: /\A[0-9\-ーぁ-んァ-ン一-龥]+\z/, message: "は数字とハイフン、漢字、ひらがな、カタカナのみ入力してください" }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数値で入力してください" }
   end
 
---
+++ spec/factories/purchase_record_shipping_destinations.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :purchase_record_shipping_destination do
+    post_code                         {'111-1111'}
+    prefecture_id                     {Faker::Number.between(from: 2, to: 48)}
+    city                              {'さいタマ市'}
+    street_address                    {'中おうク1-1-1'}
+    building_name                     {nil}
+    phone_number                      {'0000000000'}
+    association :user
+    association :item
+  end
+end
