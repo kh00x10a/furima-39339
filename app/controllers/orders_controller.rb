946c63e62615180a25b65ddfd696dcf709e25264
kh00x10a <sein.nicht04@gmail.com>
2023-05-31 10:41:06 +0900

    Formオブジェクトを用いた購入機能の実装の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,21 +2,23 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @purchase_record_shipping_destination = PurchaseRecordShippingDestination.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase_record_shipping_destination = PurchaseRecordShippingDestination.new(purchase_record_params)
     if @purchase_record_shipping_destination.valid?
       @purchase_record_shipping_destination.save
       redirect_to root_path
     else
-      render:new
+      render :index
     end
   end
 
   private
     def purchase_record_params
-      params.require(:purchase_record_shipping_destination).permit(:post_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+      params.require(:purchase_record_shipping_destination).permit(:post_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
     end
 
 end
--- app/models/purchase_record_shipping_destination.rb
+++ app/models/purchase_record_shipping_destination.rb
@@ -1,7 +1,8 @@
 class PurchaseRecordShippingDestination
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture, :city, :street_address, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :street_address, :building_name, :phone_number
     
+
   with_options presence:true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :user_id
@@ -15,6 +16,6 @@ class PurchaseRecordShippingDestination
 
   def save
     purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
-    shipping_destination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, street_address: street_address, phone_number: phone_number, purchase_record_id: purchase_record_id)
+    ShippingDestination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, street_address: street_address, phone_number: phone_number, purchase_record_id: purchase_record.id)
   end
 end
