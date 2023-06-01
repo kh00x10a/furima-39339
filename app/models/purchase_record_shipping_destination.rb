7f53bc1a677070e37b6c47c442a7aa1c7c472ca2
kh00x10a <sein.nicht04@gmail.com>
2023-06-01 14:36:58 +0900

    バリデーションのリファクタリング

--- app/models/purchase_record_shipping_destination.rb
+++ app/models/purchase_record_shipping_destination.rb
@@ -4,6 +4,7 @@ class PurchaseRecordShippingDestination
     
 
   with_options presence:true do
+    validates :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :user_id
     validates :item_id
@@ -11,8 +12,6 @@ class PurchaseRecordShippingDestination
     validates :street_address, presence: true, format: { with: /\A[0-9\-ーぁ-んァ-ン一-龥]+\z/, message: "は数字とハイフン、漢字、ひらがな、カタカナのみ入力してください" }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数値で入力してください" }
   end
-
-  validates :token, presence: true
 
   validates :prefecture_id,  numericality: { other_than: 1 , message: "can't be blank"}
 
