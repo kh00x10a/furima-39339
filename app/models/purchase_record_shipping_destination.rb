ff48e621990b9d1fbea4032bc47fb70100b8342a
kh00x10a <sein.nicht04@gmail.com>
2023-06-01 14:37:53 +0900

    バリデーションのリファクタリング2

--- app/models/purchase_record_shipping_destination.rb
+++ app/models/purchase_record_shipping_destination.rb
@@ -9,7 +9,7 @@ class PurchaseRecordShippingDestination
     validates :user_id
     validates :item_id
     validates :city, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' }
-    validates :street_address, presence: true, format: { with: /\A[0-9\-ーぁ-んァ-ン一-龥]+\z/, message: "は数字とハイフン、漢字、ひらがな、カタカナのみ入力してください" }
+    validates :street_address, format: { with: /\A[0-9\-ーぁ-んァ-ン一-龥]+\z/, message: "は数字とハイフン、漢字、ひらがな、カタカナのみ入力してください" }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数値で入力してください" }
   end
 
