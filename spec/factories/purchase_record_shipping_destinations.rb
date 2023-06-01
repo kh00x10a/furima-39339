c98bdc46105b6a056ca2a887b4eb13a33e0521e8
kh00x10a <sein.nicht04@gmail.com>
2023-06-01 14:50:32 +0900

    FactoryBotの編集

--- spec/factories/purchase_record_shipping_destinations.rb
+++ spec/factories/purchase_record_shipping_destinations.rb
@@ -6,8 +6,6 @@ FactoryBot.define do
     street_address                    {'中おうク1-1-1'}
     building_name                     {nil}
     phone_number                      {'0000000000'}
-    association :user
-    association :item
     token                             {"tok_abcdefghijk00000000000000000"}
   end
 end
